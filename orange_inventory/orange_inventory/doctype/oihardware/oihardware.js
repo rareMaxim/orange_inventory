41017d16c6ddb5888c732f50b64f5f71126ee631
Maxim S <maks4a@gmail.com>
2024-05-07 18:52:10 +0300

    feat: Model filter in Hardware

--- orange_inventory/orange_inventory/doctype/oihardware/oihardware.js
+++ orange_inventory/orange_inventory/doctype/oihardware/oihardware.js
@@ -1,8 +1,30 @@
 // Copyright (c) 2024, Maxim Sysoev and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("oiHardware", {
-// 	refresh(frm) {
+frappe.ui.form.on("oiHardware", {
+    refresh(frm) {
+        updateModelFilters(frm);
+    },
+    type(frm) {
+        updateModelFilters(frm);
+    }
+});
+function updateModelFilters(frm) {
+    frm.set_query("model", function () {
+        return {
+            "filters": getModelFilters(frm)
+        };
+    });
+};
 
-// 	},
-// });
+function getModelFilters(frm) {
+    var filters = {};
+    if (frm.doc.type) {
+        filters["type"] = frm.doc.type
+    };
+    if (frm.doc.manufacturer) {
+        filters["manufacturer"] = frm.doc.manufacturer
+    }
+    return filters;
+};
+
--- orange_inventory/orange_inventory/doctype/oihardware_model/oihardware_model.py
+++ orange_inventory/orange_inventory/doctype/oihardware_model/oihardware_model.py
@@ -6,16 +6,24 @@ from frappe.model.document import Document
 
 
 class oiHardwareModel(Document):
-	# begin: auto-generated types
-	# This code is auto-generated. Do not modify anything in this block.
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
 
-	from typing import TYPE_CHECKING
+    from typing import TYPE_CHECKING
 
-	if TYPE_CHECKING:
-		from frappe.types import DF
+    if TYPE_CHECKING:
+        from frappe.types import DF
 
-		picture: DF.AttachImage | None
-		title: DF.Data | None
-	# end: auto-generated types
+        manufacturer: DF.Link | None
+        model: DF.Data | None
+        picture: DF.AttachImage | None
+        product: DF.Data | None
+        title: DF.Data | None
+        type: DF.Link | None
+    # end: auto-generated types
 
-	pass
+    # this method will run every time a document is saved {self.model or ""}'
+    def before_save(self):
+        self.title = f"{self.manufacturer} {self.model}"
+        if self.product:
+            self.title = f"{self.title} ({self.product})"
