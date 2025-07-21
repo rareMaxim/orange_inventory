2b0bee9826742a90caad5d90a565cf2123d8cf56
Joe <mlt@mlt.gov.ua>
2025-07-21 09:55:07 +0300

    Remove obsolete oiContract and oiContract Type files and associated configurations

--- orange_inventory/orange_inventory/doctype/oicontract_type/oicontract_type.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Maxim Sysoev and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("oiContract Type", {
-// 	refresh(frm) {
-
-// 	},
-// });
