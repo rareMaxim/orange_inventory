ee333d3e6ecfc2f8927eb0298bf411ae0ad809c4
Mykola Shevhenko <jonhsmithwich@gmail.com>
2025-08-22 10:46:39 +0300

    feat: implement synchronization of network ports with assets for oiHardwareModel

--- orange_inventory/orange_inventory/doctype/oihardwaremodel/oihardwaremodel.js
+++ orange_inventory/orange_inventory/doctype/oihardwaremodel/oihardwaremodel.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2025, Maxim Sysoev and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("oiHardwareModel", {
-// 	refresh(frm) {
+frappe.ui.form.on("oiHardwareModel", {
+	refresh(frm) {
+		sync_ports_to_assets(frm);
+	},
+});
 
-// 	},
-// });
+let sync_ports_to_assets = function (frm) {
+	if (!frm.is_new() && frm.doc.is_network_device) {
+		frm.add_custom_button(__("Синхронізувати порти з активами"), function () {
+			frappe.call({
+				method: "orange_inventory.orange_inventory.doctype.oihardwaremodel.oihardwaremodel.sync_ports_to_assets",
+				args: {
+					model_name: frm.doc.name,
+				},
+				// Повідомлення буде показано з серверної частини
+			});
+		}).addClass("btn-primary");
+	}
+};
--- orange_inventory/orange_inventory/doctype/oihardwaremodel/oihardwaremodel.py
+++ orange_inventory/orange_inventory/doctype/oihardwaremodel/oihardwaremodel.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2025, Maxim Sysoev and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
@@ -27,3 +27,50 @@ class oiHardwareModel(Document):
 	# end: auto-generated types
 
 	pass
+
+
+@frappe.whitelist()
+def sync_ports_to_assets(model_name):
+	"""
+	Знаходить усі активи, що використовують цю модель, та створює для них
+	відсутні мережеві порти на основі поточних шаблонів у моделі.
+	"""
+	model_doc = frappe.get_doc("oiHardwareModel", model_name)
+	if not model_doc.is_network_device or not model_doc.port_templates:
+		frappe.msgprint("Ця модель не є мережевим пристроєм або не має шаблонів портів для синхронізації.")
+		return
+
+	# Знаходимо всі активи, пов'язані з цією моделлю
+	linked_assets = frappe.get_all("oiAsset", filters={"hardware_model": model_name}, pluck="name")
+
+	if not linked_assets:
+		frappe.msgprint("Не знайдено активів, що використовують цю модель.")
+		return
+
+	created_count = 0
+	checked_assets = 0
+	# Проходимо по кожному активу
+	for asset_name in linked_assets:
+		checked_assets += 1
+
+		# Отримуємо існуючі порти для цього активу, щоб уникнути дублікатів
+		existing_ports = frappe.get_all("oiNetworkPort", filters={"asset": asset_name}, pluck="port_name")
+
+		# Проходимо по шаблонах у моделі
+		for port_template in model_doc.port_templates:
+			# Якщо порт з такою назвою ще не існує для цього активу, створюємо його
+			if port_template.port_name not in existing_ports:
+				try:
+					new_port = frappe.new_doc("oiNetworkPort")
+					new_port.asset = asset_name
+					new_port.port_name = port_template.port_name
+					new_port.port_type = port_template.port_type
+					new_port.insert(ignore_permissions=True)
+					created_count += 1
+				except frappe.exceptions.DuplicateEntryError:
+					# На випадок рідкісних паралельних запитів, просто ігноруємо помилку
+					pass
+
+	frappe.msgprint(
+		f"Синхронізацію завершено. Перевірено {checked_assets} актив(ів). Створено {created_count} нових портів."
+	)
